34ddc8e24f115ca60619b3c1430bbb0a5b4e9c8b
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP-2.home>
2025-01-22 18:48:37 -0500

    initial commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,7 +8,7 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-//import * as logger from "firebase-functions/logger";
+// import * as logger from "firebase-functions/logger";
 import {initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
 
@@ -29,8 +29,8 @@ exports.addmessage = onRequest(async (req, res) => {
   const original = req.query.text;
   // Push the new message into Firestore using the Firebase Admin SDK.
   const writeResult = await getFirestore()
-      .collection("messages")
-      .add({original: original});
+    .collection("messages")
+    .add({original: original});
   // Send back a message that we've successfully written the message
   res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
