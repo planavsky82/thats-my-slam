c58121639e42a2f05588dd858c724c361f735aa4
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP-2.home>
2025-01-22 20:18:26 -0500

    add function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,10 +9,10 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 // import * as logger from "firebase-functions/logger";
-import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
+// import {initializeApp} from "firebase-admin/app";
+// import {getFirestore} from "firebase-admin/firestore";
 
-initializeApp();
+// initializeApp();
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -24,13 +24,18 @@ initializeApp();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
-exports.addmessage = onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await getFirestore()
-    .collection("messages")
-    .add({original: original});
-  // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
+// exports.addmessage = onRequest(async (req, res) => {
+//   // Grab the text parameter.
+//   const original = req.query.text;
+//   // Push the new message into Firestore using the Firebase Admin SDK.
+//   const writeResult = await getFirestore()
+//     .collection("messages")
+//     .add({original: original});
+//   // Send back a message that we've successfully written the message
+//   res.json({result: `Message with ID: ${writeResult.id} added.`});
+// });
+
+// public URL: https://us-central1-thats-my-slam.cloudfunctions.net/getData
+exports.getData = onRequest(async (req, res) => {
+  res.json({result: "test 123"});
 });
